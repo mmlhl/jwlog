68c7fc7976d6abf41eb13e39e2db59313d9dd3e4
mmlhl <488803459@qq.com>
2023-04-11 18:23:27 +0800

    新增能够修改端口和终结点

--- encode/encode.js
+++ encode/encode.js
@@ -1,7 +1,16 @@
-var CryptoJS = require("crypto-js");
-var http = require("http");
+const CryptoJS = require("crypto-js");
+const http = require("http");
 const url = require("url")
-var server = http.createServer(function(request, response) {
+const args = process.argv.splice(2);
+let port=5715
+let end='/encode'
+
+if (args.length === 2) {
+	port=args[0]
+	end=args[1]
+}
+
+const server = http.createServer(function (request, response) {
 	// 回调函数接收request和response对象,
 	// 获得HTTP请求的method和url:
 	console.log(request.method + ': ' + request.url);
@@ -9,22 +18,21 @@ var server = http.createServer(function(request, response) {
 	response.writeHead(200, {
 		'Content-Type': 'text/json'
 	});
-	const {query,pathname} = url.parse(request.url,true)
-	if(pathname==='/encode'){
+	const {query, pathname} = url.parse(request.url, true)
+	if (pathname === end) {
 		const n = CryptoJS.enc.Base64.parse(query.key);
-	const r=CryptoJS.DES.encrypt(query.psw, n, {
-		mode: CryptoJS.mode.ECB,
-		padding: CryptoJS.pad.Pkcs7
-	}).toString();
-	// 将HTTP响应的HTML内容写入response:
-	response.end(r);
-	}else{
+		const r = CryptoJS.DES.encrypt(query.psw, n, {
+			mode: CryptoJS.mode.ECB,
+			padding: CryptoJS.pad.Pkcs7
+		}).toString();
+		// 将HTTP响应的HTML内容写入response:
+		response.end(r);
+	} else {
 		response.end("null");
 	}
-	
+
 });
 
-// 让服务器监听8080端口:
-server.listen(5715);
+server.listen(port);
 
-console.log('Server is running at http://127.0.0.1:5715/');
+console.log('Server is running at http://127.0.0.1:'+port);
