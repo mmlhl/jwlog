749a01f6b05cb82bb002d1a96256fde7d0129f58
mmlhl <488803459@qq.com>
2023-04-11 18:23:07 +0800

    添加login文件，准备实现登录。

---
+++ login.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/11 16:56
+# @Author  : 木木
+# Love : 苏·IKUN·温柔·美丽·宝
+# @FileName: login.py
+# @Software: PyCharm
+import requests
+
+
+def ty_login(s:requests.Session):
+
+    pass
