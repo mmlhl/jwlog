58fe3db4c04ac42331e7e262c870c3eeed8f96e0
mmlhl <488803459@qq.com>
2024-06-20 19:41:35 +0800

    使用python实现加密

--- login.py
+++ login.py
@@ -3,15 +3,14 @@
 # @Author  : 木木
 # Love : 苏·IKUN·温柔·美丽·宝
 # @FileName: login.py
-# @Software: PyCharm
+# @Software: PyCharm6
 import requests
 from lxml import etree
-from encode import encode
 from config import config
-from requests.cookies import RequestsCookieJar
 import time
 import base64
-
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad
 def get_millisecond():
     return str(int(round(time.time() * 1000)))
 
@@ -27,17 +26,20 @@ def get_csrf_key_value(response_text):
 def usr_encode(acc, psw):
     return base64.b64encode(acc.encode()).decode() + '%%%' + base64.b64encode(psw.encode()).decode()
 def ty_login(acc: str, psw: str, s: requests.Session):
+    c = s.cookies.copy()
     try:
-        c = s.cookies.copy()
         header = {
             "Host": "sso.nnnu.edu.cn",
-            "Referer":"https://sso.nnnu.edu.cn/login?service=http:%2F%2Fjw.nnnu.edu.cn%2Fsso.jsp",
+            "Referer":config['other']['ty']['login_index'],
         }
         main_html_resp = s.get(config['other']['ty']['login_index'])
         html = etree.HTML(main_html_resp.text)
         login_page_flowkey = html.xpath('//p[@id="login-page-flowkey"]/text()')[0]
         login_croypto = html.xpath('//p[@id="login-croypto"]/text()')[0]
-        encode_result = encode(s, login_croypto, psw)
+        cipher = DES.new(base64.b64decode(login_croypto), DES.MODE_ECB)
+        en = cipher.encrypt(pad(psw.encode('utf-8'), DES.block_size, style='pkcs7'))
+        encode_result = base64.b64encode(en).decode('utf-8')
+
         csrf_js = s.get("https://sso.nnnu.edu.cn/public/deploy/deploy.js?" + get_millisecond())
         csrf_key, csrf_value = get_csrf_key_value(csrf_js.text)
         header["Csrf-Key"] = csrf_key
@@ -47,11 +49,9 @@ def ty_login(acc: str, psw: str, s: requests.Session):
                                     "execution": login_page_flowkey, "geolocation": "",
                                     "username": acc, "croypto": login_croypto,
                                     "password": encode_result, "passwordPre": psw}
-                              , headers=header, allow_redirects=False, timeout=3)
-
-
+                              , headers=header, allow_redirects=False, timeout=1)
         h = login_result.headers
-        if 'Location' in h.keys():
+        if not 'Location' in h.keys():
             s.headers = header['Host'] = 'jw-nnnu-edu-cn.atrust.nnnu.edu.cn'
             url = h['Location']
             while True:
@@ -70,5 +70,7 @@ def ty_login(acc: str, psw: str, s: requests.Session):
         s.cookies.update(c)
         return False
     except Exception as ee:
+        s.cookies.clear()
+        s.cookies.update(c)
         print(ee)
         return False
